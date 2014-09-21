eb383d1a86f7d74abf85506e7a2de1755c5854cc
Joseph Mann <jlatmann@gmail.com>
2014-09-21 10:51:01 -0600

    init 2
    including ui.R

--- ui.R
+++ ui.R
@@ -25,10 +25,14 @@ Aggregate Datasets by Disease - ENGLISH."),
                    motion chart, one can also filter the dataset by gender using 
                    the left-hand panel. Not choosing any gender will produce an 
                    empty chart."),
-                 p("The motion chart has a lot of functionality.")
-                 Both x and y axis
+                 p("The motion chart has a lot of functionality."),
+                 p("Both x and y axis
                    variables have many options that can be chosen by clicking on the
-                   respective labels.")
+                   respective labels."),
+                 p("The year can be chosen either with the slider, or it can be 
+                   animated with the 'play' button on the left hand side"),
+                 p("On the upper right hand side there are tabs to choose a histogram,
+                   or a line plot")
         )
             
       )
