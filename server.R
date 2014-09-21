b3ed19ea8488a952ee6b77571428d57ead61d0be
Joseph Mann <jlatmann@gmail.com>
2014-09-21 10:32:58 -0600

    initial

---
+++ server.R
@@ -0,0 +1,34 @@
+library(shiny)
+library(reshape)
+library(googleVis)
+
+chronic <- read.csv("chronic.csv")
+molten.chronic <- melt(chronic, 
+            id = c("Disease","Fiscal.Year","Gender","Age.Group"), na.rm = T)
+
+
+shinyServer(
+  function(input, output) {
+    
+    chronic.r <- reactive({
+      a <- subset(molten.chronic,
+                  Gender %in% input$gender )
+      a <- droplevels(a)
+      a <- cast(a, Disease + Fiscal.Year ~ variable, c(mean,sd))
+    
+      return(a)
+    })
+      
+
+    output$view <- renderGvis({
+      gvisMotionChart(chronic.r(),
+                      "Disease",
+                      timevar="Fiscal.Year",
+                      xvar="Population_mean",
+                      yvar="Incidence_mean",
+                      date.format="%Y")
+    })
+    
+    
+  } 
+)
---
+++ ui.R
@@ -0,0 +1,41 @@
+library(shiny)
+
+# Define UI for dataset viewer application
+shinyUI(
+  pageWithSidebar(
+    # Application title
+    headerPanel("Canadian Chronic Disease Relationships"), 
+    sidebarPanel(
+      checkboxGroupInput("gender", "Gender:",
+                         c("Male" = "M",
+                           "Female" = "F"),
+                         selected = c("M","F"))
+    ),   
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Chart",h4("Chronic Disease Chart"),htmlOutput("view")),
+        tabPanel("Data Description",
+                 p("A complete description of the variables can be found in the
+the document: Contents of the Canadian Chronic Disease Surveillance System (CCDSS) 
+Aggregate Datasets by Disease - ENGLISH."),
+                 a("http://www.phac-aspc.gc.ca/data-donnees/hpcdp-pspmc/index-eng.php")
+        ),
+        tabPanel("Usage",
+                 p("While most of the functionality of the chart comes from the 
+                   motion chart, one can also filter the dataset by gender using 
+                   the left-hand panel. Not choosing any gender will produce an 
+                   empty chart."),
+                 p("The motion chart has a lot of functionality.")
+                 Both x and y axis
+                   variables have many options that can be chosen by clicking on the
+                   respective labels.")
+        )
+            
+      )
+      
+    )
+    
+  )
+)
+
+
